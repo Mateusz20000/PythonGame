4f88057499fe1802a72cd7a2d03b91979a223f77
Mateusz20000 <rzeszotko.mateusz@gmail.com>
2025-05-15 22:23:56 +0200

    Server network side

---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+from _thread import *
+import sys
+
+server = "192.168.0.157"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+
+except socket.error as e:
+    str(e)
+
+
+
+s.listen(5)
+print("Server Started, Waiting for a connection...")
+
+def threaded_client(conn):
+    
+    replay = ""
+    while True:
+        
+        try:
+            data = conn.recv(2048)
+            replay = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+
+            else:
+                print("Recived: ", replay)
+                print("Sending: ", replay)
+            
+            conn.sendall(str.encode(replay))
+
+        except:
+            break
+
+    
+
+
+        
+
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
