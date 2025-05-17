762c508202fdda41ed8e99d93778dffef8012225
Mateusz20000 <rzeszotko.mateusz@gmail.com>
2025-05-18 00:04:44 +0200

    growing plants works but display is messed up

--- plant.py
+++ plant.py
@@ -12,20 +12,48 @@ class Sunflower:
         if self.state < 4:
             self.state += 1
 
+class Hay:
+
+    def __init__(self):
+        self.state = 1
+        self.name = "hay"
+
+    def grow(self):
+        
+        if self.state < 5:
+            self.state += 1
+    
+    def show(self):
+
+        if self.name == "hay" :
+            if self.state == 1:
+                return pygame.image.load("tiles\hay_I.png").convert()
+            if self.state == 2:
+                return pygame.image.load("tiles\hay_II.png").convert()
+            if self.state == 3:
+                return pygame.image.load("tiles\hay_III.png").convert()
+            if self.state == 4:
+                return pygame.image.load("tiles\hay_V.png").convert()
+            if self.state == 5:
+                return pygame.image.load("tiles\hay_V.png").convert()
+
+    
     
 class Tile:
 
     def show(self, Plant):
 
-        if Plant.name == "sunflower" :
+        if Plant.name == "hay" :
             if Plant.state == 1:
-                return pygame.image.load("tiles\sunflower_I.png").convert()
+                return pygame.image.load("tiles\hay_I.png").convert()
             if Plant.state == 2:
-                return pygame.image.load("tiles\sunflower_II.png").convert()
+                return pygame.image.load("tiles\hay_II.png").convert()
             if Plant.state == 3:
-                return pygame.image.load("tiles\sunflower_III.png").convert()
+                return pygame.image.load("tiles\hay_III.png").convert()
             if Plant.state == 4:
-                return pygame.image.load("tiles\sunflower_IV.png").convert()
+                return pygame.image.load("tiles\hay_V.png").convert()
+            if Plant.state == 5:
+                return pygame.image.load("tiles\hay_V.png").convert()
 
     
 def main():
@@ -34,9 +62,8 @@ def main():
     screen = pygame.display.set_mode((1920, 1080))
     clock = pygame.time.Clock()
 
-    s = Sunflower()
+    s = [Hay(), Hay(), Hay(), Hay(), Hay(), Hay(), Hay(), Hay(), Hay(), Hay()]
 
-    a = Tile()
 
     
 
@@ -50,10 +77,18 @@ def main():
                 status = False
 
 
-        if random.randint(0,100) == 100:
-            s.grow()
+        ran1 = random.randint(0,50)
 
-        screen.blit(a.show(s), (0, 0))
+        if ran1 == 1:
+            ran2 = random.randint(0,9)
+            s[ran2].grow()
+
+
+
+        for i in range(10):
+            screen.blit(s[i].show(), (0+i*32, 0+i*16))
+
+        
 
 
         pygame.display.flip()
